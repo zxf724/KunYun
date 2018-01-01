7d88112afa73be32a67b691cb09f080bedb2acad
zxf724 <565737064@qq.com>
2018-01-01 10:49:22 +0000

    demo_tcp.c

---
+++ rainboard_demo_tcp.c
@@ -0,0 +1,17 @@
+/**
+ *opyright (c) 2016 Corerain Technologies. All rights reserved. 
+ * No part of this document, either material or conceptual may be 
+ * copied or distributed, transmitted, transcribed, stored in a retrieval 
+ * system or translated into any human or computer language in
+ * any form by any means, electronic, mechanical, manual
+ * or otherwise, or disclosed to third parties without
+ * the express written permission of Corerain Technologies, 
+ * 502 Section b, 2305 Zuchongzhi Road, Zhangjiang Hi-Tech Park, 
+ * Shanghai 201203, China.
+ */
+
+/**
+ * This is the command line tool to run the integration test of LeNet 
+ */
+
+
